de83bcdfa56b679eb9a65dae7d70e456c1d1682f
BoopYurNose <zacharybaca07@gmail.com>
2026-02-25 16:46:25 -0600

    Add SellNPCInteractScript for NPC interaction

---
+++ Workspace/SellNPCInteractScript.lua
@@ -0,0 +1,7 @@
+NPCInteracted = game.ReplicatedStorage:WaitForChild("RemoteEvents"):WaitForChild("NPCInteracted")
+
+ClickDetector = script.Parent:WaitForChild("ClickDetector")
+
+ClickDetector.MouseClick:Connect(function(Player)
+	NPCInteracted:FireClient(Player, "Seller")
+end)
